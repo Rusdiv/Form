1c8dd6d57bcf4e3f0d56c095b785275d07f97cde
Kezhich <azerot96@gmail.com>
2021-03-22 01:06:30 +0300

    bar chart aspect ratio fixed

--- src/components/Chart/Graph.vue
+++ src/components/Chart/Graph.vue
@@ -1,5 +1,5 @@
 <template>
-  <canvas id="planet-chart" width="400" height="100"></canvas>
+	<canvas id="planet-chart" width="100%" height="100%"></canvas>
 </template>
 
 <script>
@@ -7,24 +7,27 @@ import Chart from 'chart.js';
 import planetChartData from './planet-data.js';
 
 export default {
-  name: 'PlanetChart',
-  data() {
-    return {
-      planetChartData: planetChartData
-    };
-  },
-  mounted() {
-    const ctx = document.getElementById('planet-chart');
-    new Chart(ctx, this.planetChartData, {
-      responsive: true,
-      maintainAspectRatio: true
-    });
-  }
+	name: 'PlanetChart',
+	data() {
+		return {
+			planetChartData: planetChartData,
+		};
+	},
+	mounted() {
+		const ctx = document.getElementById('planet-chart');
+		const parentHeight = ctx.parentNode.parentElement.clientHeight;
+		ctx.style.height = `${parentHeight}px`;
+		console.log(parentHeight);
+		new Chart(ctx, this.planetChartData, {
+			responsive: true,
+			maintainAspectRatio: false,
+		});
+	},
 };
 </script>
 
 <style>
-#planet-chart {
-  height: 100%;
-}
+	#planet-chart {
+		height: 100%;
+	}
 </style>
